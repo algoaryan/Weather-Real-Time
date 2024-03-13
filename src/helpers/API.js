edc34f53548bf4858592e148d7643d2bc07f47cd
Aryan Anshuman <105481815+sudodevop@users.noreply.github.com>
2024-03-13 20:08:29 +0530

    Update API.js

--- src/helpers/API.js
+++ src/helpers/API.js
@@ -1,3 +1,3 @@
-export const weatherAppAPI = "02658aceb2f1057cfda44df327bf46f9";
+export const weatherAppAPI = "AIzaSyAq15HbfCRMW7RqNb5LUNyOLyfzpYI0wl4";
 
 export const googleMapAPI = "AIzaSyAq15HbfCRMW7RqNb5LUNyOLyfzpYI0wl4";
