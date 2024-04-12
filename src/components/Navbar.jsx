fe16608eeafd0975853d9cf583498166ec98b20d
algoaryan <algoaryan>
2024-04-12 13:53:24 +0530

    Update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,80 +6,92 @@ import { HiLocationMarker } from "react-icons/hi";
 import { FaGithub, FaLinkedin } from "react-icons/fa";
 
 export const Navbar = () => {
+  const [city, setCity] = useState("");
+  const dispatch = useDispatch();
+  const toast = useToast();
 
-    const [city, setCity] = useState("");
-    const dispatch = useDispatch();
-    const toast = useToast();
+  const handleChnage = () => {
+    dispatch(getWeatherByCity(city, toast));
+  };
 
-    const handleChnage = () => {
-        dispatch(getWeatherByCity(city, toast));
-    }
+  const handleLocationData = () => {
+    dispatch(getWeatherByLocation(toast));
+  };
 
-    const handleLocationData = () => {
-        dispatch(getWeatherByLocation(toast));
-    }
-
-    return (
-        <Flex p={'10px'} minH={'70px'} bg={'#d7defa'} justifyContent={'center'} flexDirection={['column', 'row']} gap={['10px', '0px']}>
-            <Center px={'10px'}>
-                <Input
-                    onKeyPress={({ key }) => { key === "Enter" ? handleChnage() : undefined }}
-                    onInput={(e) => { setCity(e.target.value) }}
-                    value={city}
-                    borderRadius={'15px 0px 0px 15px'}
-                    bg={'white'}
-                    _focus={{ 'border': 'none' }}
-                    placeholder="City"
-                />
-                <Button
-                    onClick={handleChnage}
-                    borderRadius={'0px 15px 15px 0px'}
-                    color={'white'}
-                    bg={'#5e82f4'}
-                    _hover={{ 'bg': '5e82f4' }}
-                >
-                    Search
-                </Button>
-            </Center>
-            <Center px={'10px'}>
-                <Button
-                    bg={'#5e82f4'}
-                    _hover={{ 'bg': '5e82f4' }}
-                    color={'white'}
-                    w={'100%'}
-                    borderRadius={'15px'}
-                    leftIcon={<Icon w={'30px'} h={'30px'} as={HiLocationMarker} />}
-                    onClick={handleLocationData}
-                >
-                    Your Location Weather
-                </Button>
-            </Center>
-            <Center px={'10px'}>
-                <Button
-                    bg={'#5e82f4'}
-                    _hover={{ 'bg': '5e82f4' }}
-                    color={'white'}
-                    w={'100%'}
-                    borderRadius={'15px'}
-                    leftIcon={<Icon w={'30px'} h={'30px'} as={FaGithub} />}
-                    
-                >
-                    <a href="https://www.github.com/sudodevop" target="_blank">Github</a>
-                </Button>
-            </Center>
-            <Center px={'10px'}>
-                <Button
-                    bg={'#5e82f4'}
-                    _hover={{ 'bg': '5e82f4' }}
-                    color={'white'}
-                    w={'100%'}
-                    borderRadius={'15px'}
-                    leftIcon={<Icon w={'30px'} h={'30px'} as={FaLinkedin} />}
-                    
-                >
-                    <a href="https://www.linkedin.com/in/sudodevop" target="_blank">LinkedIn</a>
-                </Button>
-            </Center>
-        </Flex >
-    );
+  return (
+    <Flex
+      p={"10px"}
+      minH={"70px"}
+      bg={"#d7defa"}
+      justifyContent={"center"}
+      flexDirection={["column", "row"]}
+      gap={["10px", "0px"]}
+    >
+      <Center px={"10px"}>
+        <Input
+          onKeyPress={({ key }) => {
+            key === "Enter" ? handleChnage() : undefined;
+          }}
+          onInput={(e) => {
+            setCity(e.target.value);
+          }}
+          value={city}
+          borderRadius={"15px 0px 0px 15px"}
+          bg={"white"}
+          _focus={{ border: "none" }}
+          placeholder="City"
+        />
+        <Button
+          onClick={handleChnage}
+          borderRadius={"0px 15px 15px 0px"}
+          color={"white"}
+          bg={"#5e82f4"}
+          _hover={{ bg: "5e82f4" }}
+        >
+          Search
+        </Button>
+      </Center>
+      <Center px={"10px"}>
+        <Button
+          bg={"#5e82f4"}
+          _hover={{ bg: "5e82f4" }}
+          color={"white"}
+          w={"100%"}
+          borderRadius={"15px"}
+          leftIcon={<Icon w={"30px"} h={"30px"} as={HiLocationMarker} />}
+          onClick={handleLocationData}
+        >
+          Your Location Weather
+        </Button>
+      </Center>
+      <Center px={"10px"}>
+        <Button
+          bg={"#5e82f4"}
+          _hover={{ bg: "5e82f4" }}
+          color={"white"}
+          w={"100%"}
+          borderRadius={"15px"}
+          leftIcon={<Icon w={"30px"} h={"30px"} as={FaGithub} />}
+        >
+          <a href="https://www.github.com/algoaryan" target="_blank">
+            Github
+          </a>
+        </Button>
+      </Center>
+      <Center px={"10px"}>
+        <Button
+          bg={"#5e82f4"}
+          _hover={{ bg: "5e82f4" }}
+          color={"white"}
+          w={"100%"}
+          borderRadius={"15px"}
+          leftIcon={<Icon w={"30px"} h={"30px"} as={FaLinkedin} />}
+        >
+          <a href="https://www.linkedin.com/in/algoaryan" target="_blank">
+            LinkedIn
+          </a>
+        </Button>
+      </Center>
+    </Flex>
+  );
 };
